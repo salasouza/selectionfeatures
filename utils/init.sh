73a5fe55bfe5c38e2c23f51d67ad3ce42911f384
Salatiel Souza <sal.souzasa@gmail.com>
2023-12-04 17:43:45 -0300

    Changes perfomed on 2023-12-04 : 17-43

---
+++ utils/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " "
+echo -e "\e[1;31m Loading Conda Enviroment...! \e[0m"
+echo " "
+
+conda activate nlp
+
+echo " "
+echo -e "\e[1;31m Done! \e[0m"
+echo " "
---
+++ utils/lab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " "
+echo -e "\e[1;31m Loading jupyter...! \e[0m"
+echo " "
+
+jupyter lab --no-browser
+
+echo " "
+echo -e "\e[1;31m Jupyter stopped! \e[0m"
+echo " "
